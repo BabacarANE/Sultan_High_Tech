0a1981670918427c667cbacd5c943dc6b7de4852
BabacarANE <anebabacar221@gmail.com>
2024-07-23 16:16:03 +0000

    get all products

--- frontend/src/views/uikit/List.vue
+++ frontend/src/views/uikit/List.vue
@@ -1,44 +1,31 @@
 <script setup>
 import { ref, onMounted } from 'vue';
-import { ProductService } from '@/service/ProductService';
+import axios from 'axios';
 
-const picklistValue = ref([
-    [
-        { name: 'San Francisco', code: 'SF' },
-        { name: 'London', code: 'LDN' },
-        { name: 'Paris', code: 'PRS' },
-        { name: 'Istanbul', code: 'IST' },
-        { name: 'Berlin', code: 'BRL' },
-        { name: 'Barcelona', code: 'BRC' },
-        { name: 'Rome', code: 'RM' }
-    ],
-    []
-]);
 
-const orderlistValue = ref([
-    { name: 'San Francisco', code: 'SF' },
-    { name: 'London', code: 'LDN' },
-    { name: 'Paris', code: 'PRS' },
-    { name: 'Istanbul', code: 'IST' },
-    { name: 'Berlin', code: 'BRL' },
-    { name: 'Barcelona', code: 'BRC' },
-    { name: 'Rome', code: 'RM' }
-]);
 
-const dataviewValue = ref(null);
+
+const dataviewValue = ref([]);
 const layout = ref('grid');
 const sortKey = ref(null);
 const sortOrder = ref(null);
 const sortField = ref(null);
 const sortOptions = ref([
-    { label: 'Price High to Low', value: '!price' },
-    { label: 'Price Low to High', value: 'price' }
+    { label: 'Price High to Low', value: '!prix' },
+    { label: 'Price Low to High', value: 'prix' }
 ]);
 
-const productService = new ProductService();
+const fetchProducts = async () => {
+    try {
+        const response = await axios.get('http://127.0.0.1:8000/api/products');
+        dataviewValue.value = response.data;
+    } catch (error) {
+        console.error('Error fetching products:', error);
+    }
+};
 
 onMounted(() => {
-    productService.getProductsSmall().then((data) => (dataviewValue.value = data));
+    fetchProducts();
 });
 
 const onSortChange = (event) => {
@@ -57,18 +44,12 @@ const onSortChange = (event) => {
 };
 
 const getSeverity = (product) => {
-    switch (product.inventoryStatus) {
-        case 'INSTOCK':
-            return 'success';
-
-        case 'LOWSTOCK':
-            return 'warning';
-
-        case 'OUTOFSTOCK':
-            return 'danger';
-
-        default:
-            return null;
+    if (product.quantite_en_stock > 10) {
+        return 'success';
+    } else if (product.quantite_en_stock <= 10 && product.quantite_en_stock > 0) {
+        return 'warning';
+    } else {
+        return 'danger';
     }
 };
 </script>
@@ -95,14 +76,14 @@ const getSeverity = (product) => {
                             <div v-for="(item, index) in slotProps.items" :key="index" class="col-12">
                                 <div class="flex flex-column sm:flex-row sm:align-items-center p-4 gap-3" :class="{ 'border-top-1 surface-border': index !== 0 }">
                                     <div class="md:w-10rem relative">
-                                        <img class="block xl:block mx-auto border-round w-full" :src="`https://primefaces.org/cdn/primevue/images/product/${item.image}`" :alt="item.name" />
+                                        <img class="block xl:block mx-auto border-round w-full" :src="`http://127.0.0.1:8000/storage/${item.photo}`" :alt="item.nom" />
                                         <Tag :value="item.inventoryStatus" :severity="getSeverity(item)" class="absolute" style="left: 4px; top: 4px"></Tag>
                                     </div>
                                     <div class="flex flex-column md:flex-row justify-content-between md:align-items-center flex-1 gap-4">
                                         <div class="flex flex-row md:flex-column justify-content-between align-items-start gap-2">
                                             <div>
-                                                <span class="font-medium text-secondary text-sm">{{ item.category }}</span>
-                                                <div class="text-lg font-medium text-900 mt-2">{{ item.name }}</div>
+                                                <span class="font-medium text-secondary text-sm">{{ item.category_id }}</span>
+                                                <div class="text-lg font-medium text-900 mt-2">{{ item.nom }}</div>
                                             </div>
                                             <div class="surface-100 p-1" style="border-radius: 30px">
                                                 <div class="surface-0 flex align-items-center gap-2 justify-content-center py-1 px-2" style="border-radius: 30px; box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 2px 0px rgba(0, 0, 0, 0.06)">
@@ -112,10 +93,10 @@ const getSeverity = (product) => {
                                             </div>
                                         </div>
                                         <div class="flex flex-column md:align-items-end gap-5">
-                                            <span class="text-xl font-semibold text-900">${{ item.price }}</span>
+                                            <span class="text-xl font-semibold text-900">{{ item.prix }}</span>
                                             <div class="flex flex-row-reverse md:flex-row gap-2">
                                                 <Button icon="pi pi-heart" outlined></Button>
-                                                <Button icon="pi pi-shopping-cart" label="Buy Now" :disabled="item.inventoryStatus === 'OUTOFSTOCK'" class="flex-auto md:flex-initial white-space-nowrap"></Button>
+                                                <Button icon="pi pi-shopping-cart" label="Buy Now" :disabled="item.quantite_en_stock === 0" class="flex-auto md:flex-initial white-space-nowrap"></Button>
                                             </div>
                                         </div>
                                     </div>
@@ -130,15 +111,15 @@ const getSeverity = (product) => {
                                 <div class="p-4 border-1 surface-border surface-card border-round flex flex-column">
                                     <div class="surface-50 flex justify-content-center border-round p-3">
                                         <div class="relative mx-auto">
-                                            <img class="border-round w-full" :src="`https://primefaces.org/cdn/primevue/images/product/${item.image}`" :alt="item.name" style="max-width: 300px" />
+                                            <img class="border-round w-full" :src="`http://127.0.0.1:8000/storage/${item.photo}`" :alt="item.nom" style="max-width: 300px" />
                                             <Tag :value="item.inventoryStatus" :severity="getSeverity(item)" class="absolute" style="left: 4px; top: 4px"></Tag>
                                         </div>
                                     </div>
                                     <div class="pt-4">
                                         <div class="flex flex-row justify-content-between align-items-start gap-2">
                                             <div>
-                                                <span class="font-medium text-secondary text-sm">{{ item.category }}</span>
-                                                <div class="text-lg font-medium text-900 mt-1">{{ item.name }}</div>
+                                                <span class="font-medium text-secondary text-sm">{{ item.category_id }}</span>
+                                                <div class="text-lg font-medium text-900 mt-1">{{ item.nom }}</div>
                                             </div>
                                             <div class="surface-100 p-1" style="border-radius: 30px">
                                                 <div class="surface-0 flex align-items-center gap-2 justify-content-center py-1 px-2" style="border-radius: 30px; box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 2px 0px rgba(0, 0, 0, 0.06)">
@@ -148,9 +129,9 @@ const getSeverity = (product) => {
                                             </div>
                                         </div>
                                         <div class="flex flex-column gap-4 mt-4">
-                                            <span class="text-2xl font-semibold text-900">${{ item.price }}</span>
+                                            <span class="text-2xl font-semibold text-900">{{ item.prix }}</span>
                                             <div class="flex gap-2">
-                                                <Button icon="pi pi-shopping-cart" label="Buy Now" :disabled="item.inventoryStatus === 'OUTOFSTOCK'" class="flex-auto white-space-nowrap"></Button>
+                                                <Button icon="pi pi-shopping-cart" label="Buy Now" :disabled="item.quantite_en_stock === 0" class="flex-auto white-space-nowrap"></Button>
                                                 <Button icon="pi pi-heart" outlined></Button>
                                             </div>
                                         </div>
@@ -163,29 +144,8 @@ const getSeverity = (product) => {
             </div>
         </div>
 
-        <div class="col-12 lg:col-8">
-            <div class="card">
-                <h5>PickList</h5>
-                <PickList v-model="picklistValue" listStyle="height:250px" dataKey="code">
-                    <template #sourceheader> From </template>
-                    <template #targetheader> To </template>
-                    <template #item="slotProps">
-                        <div>{{ slotProps.item.name }}</div>
-                    </template>
-                </PickList>
-            </div>
-        </div>
 
-        <div class="col-12 lg:col-4">
-            <div class="card">
-                <h5>OrderList</h5>
-                <OrderList v-model="orderlistValue" listStyle="height:250px" dataKey="code" :rows="10">
-                    <template #header> Cities </template>
-                    <template #item="slotProps">
-                        <div>{{ slotProps.item.name }}</div>
-                    </template>
-                </OrderList>
-            </div>
-        </div>
     </div>
 </template>
+
+
